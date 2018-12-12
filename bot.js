0157ed42a6e39621199da45d4c229e1fa7594df7
zakrwn <45009413+zakrwn@users.noreply.github.com>
2018-12-12 23:33:37 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const devs = ["467402004981874713"]; //الايدي حقك
-const adminprefix = ["+"]; // البرفكس
+const adminprefix = ["T"]; // البرفكس
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
